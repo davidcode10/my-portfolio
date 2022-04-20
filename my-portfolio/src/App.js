88e58e3a86cada9c0d58baa4f42a44dea2ab2255
davidcode10 <davidedson2030@outlook.com>
2022-04-20 12:37:05 +0200

    first part added

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/header/navbar"
+import AboutSection from "./components/page/AboutSection/AboutSection"
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <NavBar />
+      </div>
+      <div>
+        <AboutSection />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ my-portfolio/src/components/page/AboutSection/AboutSection.js
@@ -0,0 +1,22 @@
+import './AboutSection.css'
+
+function AboutSection() {
+    return (
+        <section className='About-Section'>
+            <div className='cheers'>
+                <h1>Hi, my name is</h1>
+            </div>
+            <div>
+                <h2 className='name'>David.</h2>
+            </div>
+            <div>
+                <h3 className='my-work'>I'm a web developer.</h3>
+            </div>
+            <div>
+                <p>Add text...</p>
+            </div>
+        </section>
+    )
+}
+
+export default AboutSection
